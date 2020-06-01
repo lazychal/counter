c18e18df31c9da0a2bc8be519af496a8ba37e098
lazychal <disposables@yandex.ru>
2020-06-01 14:52:41 +0300

    Updates

---
+++ precache-manifest.8000c8dd352e7bad6c896159878ffee3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025288fc41681c8c3367432ddf797608",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "c78ef2e6882b60f81b2a",
+    "url": "/counter/static/css/main.3a9a2145.chunk.css"
+  },
+  {
+    "revision": "551baa139ce25c6cb61e",
+    "url": "/counter/static/js/2.9e127fdf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/counter/static/js/2.9e127fdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c78ef2e6882b60f81b2a",
+    "url": "/counter/static/js/main.9b428848.chunk.js"
+  },
+  {
+    "revision": "2e0233fda58cf534668a",
+    "url": "/counter/static/js/runtime-main.3700766e.js"
+  }
+]);
